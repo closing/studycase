bd248df02bd9f7cf763539068293156a780f37fe
closing <zhu.yuxing@hotmail.com>
2018-07-05 14:36:51 +0900

    Create WebSocketConfig.java

---
+++ spring-boot-demo/src/main/java/com/example/springbootdemo/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.example.springbootdemo.config;
+
+//WebSocket2
+//@Configuration
+//@EnableWebSocket
+public class WebSocketConfig //implements WebSocketConfigurer
+{
+
+	//	@Override
+	//	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+	//
+	//		registry.addHandler(webSocketHandler(), "/webSocketServer/*");
+	//	}
+	//
+	//	@Bean
+	//	public WebSocketHandler webSocketHandler() {
+	//		return new WebSocketServer2();
+	//
+	//	}
+
+}
