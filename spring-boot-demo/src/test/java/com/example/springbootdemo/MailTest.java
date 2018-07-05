0ed83b731a4cedd2d6e4c544d86c36c3323dfc00
closing <zhu.yuxing@hotmail.com>
2018-07-05 14:31:46 +0900

    Create MailTest.java

---
+++ spring-boot-demo/src/test/java/com/example/springbootdemo/MailTest.java
@@ -0,0 +1,14 @@
+package com.example.springbootdemo;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class MailTest {
+
+	//	@Autowired
+	//	private JavaMailComponent javaMailComponent;
+	//
+	//	@Test
+	//	public void test() {
+	//		this.javaMailComponent.sendMail("xx@xx.com");
+	//	}
+}
