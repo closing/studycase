db8cc1049686c0c190064b8e1e4aa7e7f1c51dcd
closing <zhu.yuxing@hotmail.com>
2018-07-05 14:35:43 +0900

    Create GlobalDefaultExceptionHandler.java

---
+++ spring-boot-demo/src/main/java/com/example/springbootdemo/aop/GlobalDefaultExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.example.springbootdemo.aop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalDefaultExceptionHandler {
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	public Map<String, Object> defaultExceptionHandler(Exception e) {
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("code", 500);
+		map.put("msg", e.getMessage());
+		return map;
+	}
+
+}
