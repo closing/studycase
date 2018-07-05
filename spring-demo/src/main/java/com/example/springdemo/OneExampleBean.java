f4811ca10a220a9ebc9d2e8649907f7c4ec5317e
closing <zhu.yuxing@hotmail.com>
2018-07-05 14:25:31 +0900

    Create OneExampleBean.java

---
+++ spring-demo/src/main/java/com/example/springdemo/OneExampleBean.java
@@ -0,0 +1,32 @@
+package com.example.springdemo;
+
+public class OneExampleBean {
+	private int i;
+	private AnotherExampleBean anotherExample;
+	private YetAnotherExampleBean yetAnothorExample;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public AnotherExampleBean getAnotherExample() {
+		return anotherExample;
+	}
+
+	public void setAnotherExample(AnotherExampleBean anotherExample) {
+		this.anotherExample = anotherExample;
+	}
+
+	public YetAnotherExampleBean getYetAnothorExample() {
+		return yetAnothorExample;
+	}
+
+	public void setYetAnothorExample(YetAnotherExampleBean yetAnothorExample) {
+		this.yetAnothorExample = yetAnothorExample;
+	}
+
+}
