6acfa084f7a8791b80f90d2d72c90befd5b192cc
closing <zhu.yuxing@hotmail.com>
2018-07-05 14:23:08 +0900

    Create MyMethodInterceptor.java

---
+++ spring-demo/src/main/java/com/example/springdemo/proxy/MyMethodInterceptor.java
@@ -0,0 +1,16 @@
+package com.example.springdemo.proxy;
+
+import java.lang.reflect.Method;
+
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+public class MyMethodInterceptor implements MethodInterceptor {
+
+	@Override
+	public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable {
+		System.out.println("CGLib Proxy:Hello!");
+		return arg3.invokeSuper(arg0, arg2);
+	}
+
+}
