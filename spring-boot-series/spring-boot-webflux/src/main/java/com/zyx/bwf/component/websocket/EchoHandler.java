67ebd6a2495e142ab6a042399c6c4fdfbee542e6
closing <zhu.yuxing@hotmail.com>
2018-07-10 10:08:05 +0900

    Create EchoHandler.java

---
+++ spring-boot-series/spring-boot-webflux/src/main/java/com/zyx/bwf/component/websocket/EchoHandler.java
@@ -0,0 +1,18 @@
+package com.zyx.bwf.component.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketSession;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class EchoHandler implements WebSocketHandler {
+
+	@Override
+	public Mono<Void> handle(WebSocketSession session) {
+		// TODO WebSocket is not
+		return session.send(session.receive().map(msg -> session.textMessage("ECHO -> " + msg.getPayloadAsText())));
+	}
+
+}
