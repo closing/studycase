ff083b9a8c5956a55283047aa8a0a85bebae2149
closing <zhu.yuxing@hotmail.com>
2018-07-10 10:08:58 +0900

    Create BasicController.java

---
+++ spring-boot-series/spring-boot-webflux/src/main/java/com/zyx/bwf/controller/BasicController.java
@@ -0,0 +1,14 @@
+package com.zyx.bwf.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class BasicController {
+	@GetMapping("/hello")
+	public Mono<String> sayHelloWorld() {
+		return Mono.just("Hello World");
+	}
+}
