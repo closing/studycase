deb6c54b965b2b0804b29ad0d99e9a7299b88078
closing <zhu.yuxing@hotmail.com>
2018-07-10 10:07:09 +0900

    Create SpringFrameworkWebfluxApplication.java

---
+++ spring-boot-series/spring-boot-webflux/src/main/java/com/zyx/bwf/SpringFrameworkWebfluxApplication.java
@@ -0,0 +1,12 @@
+package com.zyx.bwf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkWebfluxApplication.class, args);
+	}
+}
