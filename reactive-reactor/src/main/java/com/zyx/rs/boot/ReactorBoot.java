5b0f3dcb58b9db111c08014e599c3cda5a9fb425
closing <zhu.yuxing@hotmail.com>
2018-07-13 21:26:41 +0900

    Update ReactorBoot.java

--- reactive-reactor/src/main/java/com/zyx/rs/boot/ReactorBoot.java
+++ reactive-reactor/src/main/java/com/zyx/rs/boot/ReactorBoot.java
@@ -1,6 +1,7 @@
 package com.zyx.rs.boot;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.zyx.rs.subscriber.SampleSubscriber;
@@ -74,6 +75,36 @@ public class ReactorBoot {
 				s.next(result);
 			}
 		}).subscribe(System.out::println);
+
+		System.out.println("================");
+//		Flux.range(1, 10000).publishOn(Schedulers.parallel())
+//				.subscribe(i -> System.out.println(i));
+
+		System.out.println("================");
+		Flux.interval(Duration.ofMillis(250)).map(input -> {
+			if (input < 3)
+				return "ticket" + input;
+			throw new RuntimeException("boom");
+		}).onErrorReturn("Uh oh").subscribe(System.out::println);
+
+		System.out.println("================");
+//		Flux.interval(Duration.ofMillis(250)).map(input -> {
+//			if (input < 3)
+//				return "ticket" + input;
+//			throw new RuntimeException("boom");
+//		}).elapsed().retry(1).subscribe(System.out::println);
+
+		System.out.println("================");
+//		Flux.<String> error(new IllegalArgumentException()).doOnError(System.out::println)
+//				.retryWhen(companion -> companion.take(3));
+
+		System.out.println("================");
+		Flux.just("foo").map(input -> {
+			throw new IllegalArgumentException(input);
+		}).subscribe(v -> System.out.println("GOT VALUE"),
+				e -> System.out.println("ERROR: " + e));
+
+		System.in.read();
 
 	}
 
