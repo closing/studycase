8344b73b921717b0122d45924231025b8974b2eb
closing <zhu.yuxing@hotmail.com>
2018-09-04 10:12:26 +0900

    Update OrcWebApplication.java

--- spring-cloud-series/races/ocr-web/src/main/java/com/zyx/spring/cloud/ocr/web/OrcWebApplication.java
+++ spring-cloud-series/races/ocr-web/src/main/java/com/zyx/spring/cloud/ocr/web/OrcWebApplication.java
@@ -2,8 +2,12 @@ package com.zyx.spring.cloud.ocr.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,5 +16,15 @@ public class OrcWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrcWebApplication.class, args);
+	}
+		@Bean
+	public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<HystrixMetricsStreamServlet>(
+				streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
 	}
 }
